e7a53841b43562be6e7b8b6f46a64612f0e119dc
Lasse-94 <laab5@student.eadania.dk>
2026-04-08 17:09:09 +0200

    ændring

--- clean.R
+++ clean.R
@@ -12,10 +12,21 @@ subscription <- subscription %>% distinct(pseudo_id, .keep_all = TRUE)
 # Flet data + churn-definition
 merged_data <- subscription %>%
   left_join(cancellation, by = "pseudo_id") %>%
+  
+  # Parse dates
+  mutate(
+    subscription_cancel_date = dmy(subscription_cancel_date),
+    first_campaign_day = dmy(first_campaign_day),
+    last_campaign_day = dmy(last_campaign_day),
+    expiration_date = ymd(expiration_date)
+  ) %>%
+  
+  # Definer churn efter kampagneperioden
   mutate(
     churn = if_else(
-      subscription_cancel_date != "01-01-3000" &
-        !is.na(subscription_cancel_date),
+      !is.na(subscription_cancel_date) &
+        subscription_cancel_date < as.Date("2999-01-01") &
+        subscription_cancel_date > last_campaign_day,
       1, 0
     )
   )
@@ -24,18 +35,13 @@ merged_data <- subscription %>%
 model_data <- merged_data %>%
   select(-pseudo_id) %>%
   
-  # Dato parsing (ensartede formater)
   mutate(
-    subscription_cancel_date = dmy(subscription_cancel_date),
-    expiration_date = ymd(expiration_date),
     order_date = as.Date(ymd_hms(order_date)),
     birthdate = as.Date(parse_date_time(birthdate, orders = c("dmy", "ymd", "mdy"))),
-    usr_created = dmy(usr_created),
-    first_campaign_day = dmy(first_campaign_day),
-    last_campaign_day = dmy(last_campaign_day)
+    usr_created = dmy(usr_created)
   ) %>%
   
-  # Fjern NA tidligt (fix)
+  # Fjern NA
   filter(!is.na(birthdate), !is.na(usr_created), !is.na(order_date)) %>%
   
   # Kategorisk rensning
@@ -55,7 +61,7 @@ model_data <- merged_data %>%
     permission_given_today = factor(if_else(permission_given_today == "true", 1, 0))
   ) %>%
   
-  # Afledte variable (først numeriske)
+  # Afledte variable
   mutate(
     age = as.numeric(Sys.Date() - birthdate) / 365,
     kundetid_dage = as.numeric(order_date - usr_created)
@@ -64,24 +70,8 @@ model_data <- merged_data %>%
   # Ekstra sikkerhed
   filter(!is.na(kundetid_dage)) %>%
   
-  # Afledte kategoriske variable
+  # Kundetid grupper
   mutate(
-    aldersgruppe = case_when(
-      age < 25 ~ "Under 25",
-      age < 35 ~ "25–34",
-      age < 45 ~ "35–44",
-      age < 55 ~ "45–54",
-      age < 65 ~ "55–64",
-      TRUE ~ "65+"
-    ) %>% factor(levels = c(
-      "Under 25",
-      "25–34",
-      "35–44",
-      "45–54",
-      "55–64",
-      "65+"
-    )),
-    
     kundetid_gruppe = case_when(
       kundetid_dage <= 7 ~ "0-7 dage",
       kundetid_dage <= 30 ~ "8-30 dage",
@@ -94,10 +84,9 @@ model_data <- merged_data %>%
 model_data <- model_data %>%
   select(
     -kundetid_dage,
-    -age,
     -order_date,
     -birthdate,
-    -usr_created,
+    -usr_created
   )
 
 # Tjek
@@ -105,6 +94,7 @@ glimpse(model_data)
 summary(model_data$age)
 colSums(is.na(model_data))
 
+# Churn distribution
 model_data %>%
   count(churn) %>%
   mutate(prop = n / sum(n))
